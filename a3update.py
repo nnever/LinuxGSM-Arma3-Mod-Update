606c55ed99b60577e81d27babb5e8e27bdc98678
nnever <revennstone1@gmail.com>
2024-12-06 00:20:42 -0800

    Changed a lot.

--- a3update.py
+++ a3update.py
@@ -2,158 +2,146 @@
 
 import os
 import os.path
+import sys
 import re
 import shutil
-import time
 import getpass
-from bs4 import BeautifulSoup
-
+from pathlib import Path
 from datetime import datetime
 from urllib import request
+from argparse import ArgumentParser
+from bs4 import BeautifulSoup
 
 #region Configuration
-A3_SERVER_FOLDER = "srv2"
+
+parser = ArgumentParser()
+parser.add_argument('-u', "--updateall", action="store_true", help="Update game and mods")
+parser.add_argument('-ug', "--updategame", action="store_true", help="Only update mods")
+parser.add_argument('-um', "--updatemods", action="store_true", help="Only update game")
+parser.add_argument('-s', "--startserver", action="store_true", help="Run server")
+
 A3_SERVER_ID = "233780"
-A3_SERVER_DIR = "/home/gsm/games/gsm/arma/{}".format(A3_SERVER_FOLDER)
 A3_WORKSHOP_ID = "107410"
+STEAM_CMD = Path(Path.cwd(), "steam/steamcmd.sh")
+A3_GAME_INSTALL_DIR = Path(Path.cwd(), "arma3/install/")
+A3_SERVER_DIR = Path(Path.cwd(), "arma3/install/public")
+A3_WORKSHOP_DIR = Path(Path.cwd(),"arma3/install/steamapps/workshop/content/", A3_WORKSHOP_ID)
+A3_MODS_DIR = Path(A3_SERVER_DIR, "mods")
+A3_SERVER_CFG = Path(A3_SERVER_DIR, "serverconfig/server.cfg")
+A3_SERVER_BASIC_CFG = Path(A3_SERVER_DIR, "serverconfig/basic.cfg")
 
-STEAM_CMD = "{}/steamcmd/steamcmd.sh".format(A3_SERVER_DIR)
-STEAM_USER = input("Steam Username: ")
-STEAM_PASS = getpass.getpass(prompt="Steam Password ")
+LAUNCH_PARAMETERS = (
+    f"-cfg=\"{A3_SERVER_BASIC_CFG}\" "
+    f"-config=\"{A3_SERVER_CFG}\" "
+    f"-name=\"public\" "
+    f"-world=empty "
+    f"-port=2302 "
+    f"-noSound"
+)
 
-A3_WORKSHOP_DIR = "{}/steamapps/workshop/content/{}".format(A3_SERVER_DIR, A3_WORKSHOP_ID)
-A3_MODS_DIR = "{}/serverfiles/mods".format(A3_SERVER_DIR)
-
-print("")
-print("Select Modlist Files Found")
-f = []
-for (dirpath, dirnames, filenames) in os.walk("{}/modlists".format(A3_SERVER_DIR)):
-    f.extend(filenames)
-    break
-i = 1
-f.sort()
-for file in f:
-    print("{}.) {}".format(i, file))
-    i += 1
-print("")
-
-selected_file = int(input("Enter Number: ")) - 1
-MOD_FILE = "{}/modlists/{}".format(A3_SERVER_DIR, f[selected_file])
-
-if re.search("\.html$", MOD_FILE) == False:
-    MOD_FILE = "{}.html".format(MOD_FILE)
-
-
-if os.path.exists(MOD_FILE) == False:
-    print ("Missing Mod File {}".format(MOD_FILE))
-    sys.exit
-
-soup = BeautifulSoup(open(MOD_FILE, "r").read(), features="lxml")
-
-MODS = {}
-for item in soup.findAll("tr", {"data-type" : "ModContainer"}):
-    name_link   = item.find("a", {"data-type" : "Link"})
-    name_object = item.find("td", {"data-type" : "DisplayName"})
-
-    workshopId = re.search("id=(\d+)", name_link["href"])
-
-    if id:
-        mod_id = workshopId.group(1)
-        mod_link = name_object.contents[0].lower().replace(" ", "_");
-        MODS["@{}".format(mod_link)] = mod_id
-
-PATTERN = re.compile(r"workshopAnnouncement.*?<p id=\"(\d+)\">", re.DOTALL)
-WORKSHOP_CHANGELOG_URL = "https://steamcommunity.com/sharedfiles/filedetails/changelog"
 #endregion
 
 #region Functions
-def log(msg):
+def log(msg: str):
     print("")
-    print("{{0:=<{}}}".format(len(msg)).format(""))
-    print(msg);
-    print("{{0:=<{}}}".format(len(msg)).format(""))
+    print(f"{'=' * len(msg)}")
+    print(msg)
+    print(f"{'=' * len(msg)}")
 
-
-def call_steamcmd(params):
-    os.system("{} {}".format(STEAM_CMD, params))
+def call_steamcmd(params: str):
+    os.system(f"{STEAM_CMD} {params}")
     print("")
 
+def find_modlist_html_file() -> Path:
+    modlists_dir = Path(A3_SERVER_DIR, "modlists")
+    if not modlists_dir.exists():
+        print(f"Modlist directory does not exist: {modlists_dir}")
+        sys.exit()
 
-def update_server():
-    os.system("cd {} && ./arma3server stop".format(A3_SERVER_DIR))
-    os.system("cd {} && ./arma3server update-lgsm".format(A3_SERVER_DIR))
-    os.system("cd {} && ./arma3server update".format(A3_SERVER_DIR))
+    print("Select one of the following modlists:")
+    file_list = sorted(modlists_dir.iterdir())
 
-def start_server():
-    config_mods = "";
-    for mod_name, mod_id in MODS.items():
-        config_mods += "mods/{}\;".format(re.escape(mod_name))
+    if not file_list:
+        print("No modlist html files found.")
+        sys.exit()
 
-    config_mods = "mods=\"{}\"".format(config_mods)
+    for idx, file in enumerate(file_list, start=1):
+        print(f"{idx}.) {file.name}")
 
-    replaced = False
-    f = open("{}/lgsm/config-lgsm/arma3server/arma3server.cfg".format(A3_SERVER_DIR), "w+")
-    for line in f:
-        if re.search('mods\=\".*\"', line):
-            newline = line.replace('.*', config_mods)
-            replaced = True
+    print("")
+    try:
+        if len(file_list) == 1:
+            selected_file_idx = 0
+        else:
+            selected_file_idx = int(input("Enter Number: ")) - 1
+        mod_file = file_list[selected_file_idx]
+    except (ValueError, IndexError):
+        print("Invalid selection.")
+        sys.exit()
 
-    if replaced == False:
-        f.write(config_mods)
-    #todo set mods in lgsm config
-    f.close()
-    os.system("cd {} && ./arma3server start".format(A3_SERVER_DIR))
+    if mod_file.suffix != ".html":
+        print(f"{mod_file} is not an HTML file.")
+        sys.exit()
 
+    return mod_file
 
-def mod_needs_update(mod_id, path):
-    if os.path.isdir(path):
-        response = request.urlopen("{}/{}".format(WORKSHOP_CHANGELOG_URL, mod_id)).read()
+def extract_modlist_from_html(modlist_file: Path) -> dict:
+    with open(modlist_file, "r", encoding="utf-8") as file:
+        soup = BeautifulSoup(file.read(), features="lxml")
+
+    mods = {}
+    for item in soup.findAll("tr", {"data-type" : "ModContainer"}):
+        name_link   = item.find("a", {"data-type" : "Link"})
+        name_object = item.find("td", {"data-type" : "DisplayName"})
+
+        workshop_id = re.search(r"id=(\d+)", name_link["href"])
+
+        if workshop_id:
+            mod_id = workshop_id.group(1)
+            mod_link = name_object.contents[0].lower().replace(" ", "_")
+            mods[f"@{mod_link}"] = mod_id
+    return mods
+
+def mod_needs_update(mod_id: str, path: Path) -> bool:
+    if path.exists():
+        workshop_changelog_url = "https://steamcommunity.com/sharedfiles/filedetails/changelog"
+        update_pattern = re.compile(r"workshopAnnouncement.*?<p id=\"(\d+)\">", re.DOTALL)
+        response = request.urlopen(f"{workshop_changelog_url}/{mod_id}").read()
         response = response.decode("utf-8")
-        match = PATTERN.search(response)
+        match = update_pattern.search(response)
 
         if match:
             updated_at = datetime.fromtimestamp(int(match.group(1)))
             created_at = datetime.fromtimestamp(os.path.getctime(path))
 
-            return (updated_at >= created_at)
+            return updated_at >= created_at
 
     return False
 
-
-def update_mods():
-    steam_cmd_params  = " +login {} {}".format(STEAM_USER, STEAM_PASS)
-    steam_cmd_params += " +force_install_dir {}".format(A3_SERVER_DIR)
-    i = 0;
-    for mod_name, mod_id in MODS.items():
-        path = "{}/{}".format(A3_WORKSHOP_DIR, mod_id)
-
-        # Check if mod needs to be updated
-        if os.path.isdir(path):
-
-            if mod_needs_update(mod_id, path):
-                # Delete existing folder so that we can verify whether the
-                # download succeeded
-                shutil.rmtree(path)
+def get_mod_update_list(mods: dict) -> dict:
+    mods_to_update = {}
+    for mod_name, mod_id in mods.items():
+        mod_path = Path(A3_WORKSHOP_DIR, mod_id)
+        if mod_path.exists():
+            if mod_needs_update(mod_id, mod_path):
+                print(f"Update required for \"{mod_name}\" ({mod_id})")
+                shutil.rmtree(mod_path)
             else:
-                print("No update required for \"{}\" ({})... SKIPPING".format(mod_name, mod_id))
+                print(f"No update required for \"{mod_name}\" ({mod_id})... SKIPPING")
                 continue
 
-        # Keep trying until the download actually succeeded
-        print("Updating \"{}\" ({})".format(mod_name, mod_id))
+        mods_to_update[mod_name] = mod_id
 
-        steam_cmd_params += " +workshop_download_item {} {} validate ".format(
-            A3_WORKSHOP_ID,
-            mod_id
-        )
-        i = i + 1
-    if i > 0:
-       steam_cmd_params += " +quit"
-       call_steamcmd(steam_cmd_params)
-    else:
-       print("All Mods are up to date!")
-    return True;
+    return mods_to_update
 
-def lowercase_workshop_dir():
+def update_mods(mods_to_update: dict, username: str, password: str) -> None:
+    steam_cmd_params  = f" +force_install_dir {A3_SERVER_DIR} +login {username} {password}"
+    for mod_name, mod_id in mods_to_update.items():
+        steam_cmd_params += f" +workshop_download_item {A3_WORKSHOP_ID} {mod_id} validate"
+        print(f"Updating \"{mod_name}\" ({mod_id})")
+    call_steamcmd(f"{steam_cmd_params}  +quit")
+
+def lowercase_workshop_dir() -> None:
     def rename_all( root, items):
         for name in items:
             try:
@@ -168,31 +156,109 @@ def lowercase_workshop_dir():
         rename_all(root, files)
 
 
-def create_mod_symlinks():
-    for mod_name, mod_id in MODS.items():
-        link_path = "{}/{}".format(A3_MODS_DIR, mod_name)
-        real_path = "{}/{}".format(A3_WORKSHOP_DIR, mod_id)
+def create_mod_symlinks(mods: dict) -> None:
+    for mod_name, mod_id in mods.items():
+        link_path = f"{A3_MODS_DIR}/{mod_name}"
+        real_path = f"{A3_WORKSHOP_DIR}/{mod_id}"
 
         if os.path.isdir(real_path):
             if not os.path.islink(link_path):
                 os.symlink(real_path, link_path)
-                print("Creating symlink '{}'...".format(link_path))
+                print(f"Creating symlink '{link_path}'...")
         else:
-            print("Mod '{}' does not exist! ({})".format(mod_name, real_path))
+            print(f"Mod '{mod_name}' does not exist! ({real_path})")
+
+def update_server(username: str, password: str) -> None:
+    update_command = (
+        f"+force_install_dir {A3_GAME_INSTALL_DIR} "
+        f"+login {username} {password} "
+        f"+app_update 233780 validate "
+        f"+quit"
+        )
+
+    call_steamcmd(update_command)
+
+def generate_cfg(mods: dict) -> None:
+    config_mods = ""
+    for mod_name in mods.keys():
+        config_mods += fr"mods/{re.escape(mod_name)};"
+
+    config_mods = f"mods=\"{config_mods}\";"
+
+    with open(A3_SERVER_CFG, "r+", encoding="utf-8") as config_file:
+        lines = config_file.readlines()
+
+    replaced = False
+
+    for i, line in enumerate(lines):
+        if re.search(r'mods\=".*\"', line):
+            if lines[i].strip() != '':
+                lines[i] = f"{config_mods}"
+            else:
+                lines[i] = f"{config_mods}"
+            replaced = True
+            break
+
+    if not replaced:
+        if lines and lines[-1].strip():
+            lines.append(f"\n{config_mods}\n")
+        else:
+            lines.append(f"{config_mods}")
+
+    with open(A3_SERVER_CFG, "w", encoding="utf-8") as config_file:
+        config_file.writelines(lines)
+
+def start_server(params):
+    os.system(f"cd {A3_SERVER_DIR} && ./arma3server {params}")
 
 #endregion
 
-log("Updating A3 server ({})".format(A3_SERVER_ID))
-update_server()
+def get_credentials() -> tuple[str, str]:
+    print("A steam account owning arma3 is required to continue. Please log in to begin downloading.")
+    username = input("Steam Username: ")
+    password = getpass.getpass(prompt="Steam Password ")
+    return username, password
 
-log("Updating mods")
-update_mods()
+if __name__ == "__main__":
 
-log("Converting uppercase files/folders to lowercase...")
-lowercase_workshop_dir()
+    args = parser.parse_args()
+    START_SERVER = args.startserver
+    if args.updateall:
+        UPDATE_A3 = True
+        UPDATE_MODS = True
+    else:
+        UPDATE_A3 = args.updategame
+        UPDATE_MODS = args.updatemods
 
-log("Creating symlinks...")
-create_mod_symlinks()
+    if UPDATE_A3:
+        log(f"Updating A3 server ({A3_SERVER_ID})")
+        steam_user, steam_pass = get_credentials()
+        update_server(steam_user, steam_pass)
 
-log("Start A3 server")
-start_server()
+    elif UPDATE_MODS:
+        html_modlist = find_modlist_html_file()
+        mod_list = extract_modlist_from_html(html_modlist)
+        log("Checking for missing mods")
+        mod_update_list = get_mod_update_list(mod_list)
+
+        if mod_update_list:
+            log("Updating mods")
+            steam_user, steam_pass = get_credentials()
+            update_mods(mod_update_list, steam_user, steam_pass)
+        else:
+            print("All mods are up to-date")
+
+        log("Converting uppercase files/folders to lowercase...")
+        lowercase_workshop_dir()
+
+        log("Creating symlinks...")
+        create_mod_symlinks(mod_list)
+
+        log("Generating config file...")
+        generate_cfg(mod_list)
+    else:
+        START_SERVER = True
+
+    if START_SERVER:
+        log("Start A3 server")
+        start_server(LAUNCH_PARAMETERS)
